774fe67e8b311a327323c2f4b244cf13e3207680
Hamst3R-23 <khudobin3@gmail.com>
2023-09-08 20:59:04 +0300

    Main class and MainTest class

---
+++ src/main/java/com/example/cert_auth_service/CertAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cert_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cert_auth_service/CertAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cert_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
