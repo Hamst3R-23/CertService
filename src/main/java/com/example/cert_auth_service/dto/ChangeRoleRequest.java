4c518af9771b183a2ed0bd9dde8ded4709ce32f9
Hamst3R-23 <khudobin3@gmail.com>
2023-09-08 21:01:09 +0300

    Dto

---
+++ src/main/java/com/example/cert_auth_service/dto/ChangeRoleRequest.java
@@ -0,0 +1,24 @@
+package com.example.cert_auth_service.dto;
+
+public class ChangeRoleRequest {
+
+    long userId;
+
+    long roleId;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+}
