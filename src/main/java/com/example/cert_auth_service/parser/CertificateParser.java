db173991eec24f132bf8f4c4d7008d5b71099c54
Hamst3R-23 <khudobin3@gmail.com>
2023-09-15 23:20:10 +0300

    fixed Parser bags

--- src/main/java/com/example/cert_auth_service/parser/CertificateParser.java
+++ src/main/java/com/example/cert_auth_service/parser/CertificateParser.java
@@ -1,14 +1,15 @@
 package com.example.cert_auth_service.parser;
 
-import com.example.cert_auth_service.model.service.CertificateModelService;
+import com.example.cert_auth_service.model.service.CertificateServiceModel;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.io.InputStream;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Base64;
+
 
 public class CertificateParser {
 
@@ -16,20 +17,23 @@ public class CertificateParser {
     }
 
     private static final String SHA_256 = "SHA-256";
+    private static final String X509 = "X.509";
 
-    public static CertificateModelService Parser(String string) throws CertificateException, IOException {
+    public static CertificateServiceModel parse(String certificate) throws CertificateException {
+        InputStream targetStream = new ByteArrayInputStream(certificate.getBytes());
 
-        String toDecode = string
+        X509Certificate cert = (X509Certificate) CertificateFactory
+                .getInstance(X509)
+                .generateCertificate(targetStream);
+
+
+        String toDecode = certificate
                 .replace("-----BEGIN CERTIFICATE-----", "")
                 .replaceAll(System.lineSeparator(), "")
                 .replace("-----END CERTIFICATE-----", "");
+        String fingerprint = DigestUtils.sha256Hex(Base64.getDecoder().decode(toDecode));
 
-        ByteArrayInputStream certStream = new ByteArrayInputStream(Base64.getDecoder().decode(toDecode));
-        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-        X509Certificate cert = (X509Certificate) certFactory.generateCertificate(certStream);
-        String fingerprint = DigestUtils.sha256Hex(certStream);
-
-        return new CertificateModelService(fingerprint, SHA_256, cert.getSubjectX500Principal().toString());
+        return new CertificateServiceModel(fingerprint, SHA_256, cert.getSubjectX500Principal().toString());
     }
 
 }
